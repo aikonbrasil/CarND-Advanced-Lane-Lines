bf1c4eef18ec21651277a61d6ee9e9e7dcb82a91
Dick Carrillo Melgarejo <dcarrillom@gmail.com>
2018-02-27 18:25:47 -0300

    Adding script and updating main.py

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
+from math import fabs
 # Import everything needed to edit/save/watch video clips
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
+global objpoints
+global imgpoints
+global src
+global dst
+global globalCounter
+global left_lane_inds
+global right_lane_inds
+global left_fitx_mean
+global right_fitx_mean
+globalCounter = 0
 
 ###############################################################################
 def cal_undistort(img, objpoints, imgpoints):
@@ -146,7 +157,8 @@ def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100), d_thresh=(0, np.pi/2
     # be beneficial to replace this channel with something else.
     #color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
     color_binary = np.dstack((sxybinary, sxbinary, s_binary)) * 255
-    return color_binary, combined_binary
+    #return color_binary, combined_binary
+    return sxbinary_direction, combined_binary
 
 
 if flag_plot == True:
@@ -222,7 +234,7 @@ for frame in images:
 # PIPELINE (lane line pixels) Describe how (and identify where in your code) you
 # identified lane-line pixels and fit their positions with a polynomial?
 ###############################################################################
-def list_of_peaks(hist, maxrange):
+def list_of_peaks(hist, maxrange,flag):
     #Convert histogram to simple list
     #hist = [val[0] for val in hist];
 
@@ -243,11 +255,20 @@ def list_of_peaks(hist, maxrange):
     for ii in range(0,20):
         gradiente = s[ii][0] - s[ii+1][0]
         #print(gradiente)
-        if gradiente < -100:
+        if flag == 1:
+            if gradiente < -100:
             #print("Segundo Pico potencial util")
             #print(s[ii+1][0])
-            index_of_highest_peak = s[ii+1][0]
-            break
+                index_of_highest_peak = s[ii+1][0]
+                break
+        else:
+            if gradiente > 250:
+                print("Segundo Pico potencial util")
+                print(s)
+                print(gradiente)
+                print(s[ii+1][0])
+                index_of_highest_peak = s[ii+1][0]
+                break
 
     #index_of_second_highest_peak = s[1][0]
     #print(s)
@@ -255,7 +276,9 @@ def list_of_peaks(hist, maxrange):
     #print(index_of_highest_peak)
     return index_of_highest_peak
 
-def findng_lines(binary_warped):
+def findng_lines_slidingwindow(binary_warped):
+    global left_lane_inds
+    global right_lane_inds
     # Take a histogram of the bottom half of the image
     histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
     # Create an output image to draw on and  visualize the result
@@ -263,16 +286,18 @@ def findng_lines(binary_warped):
     # Find the peak of the left and right halves of the histogram
     # These will be the starting point for the left and right lines
     midpoint = np.int(histogram.shape[0]/2)
-    leftx_base = np.argmax(histogram[:midpoint])
-    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    leftx_base = np.argmax(histogram[400:midpoint])+400
+    rightx_base = np.argmax(histogram[midpoint:1060]) + midpoint
 
     #print('Pico raw izquerda')
     #print(leftx_base)
-
-    peak_1 = list_of_peaks(histogram[:midpoint],midpoint)
+    #print("ENTRO PARA CALCULAR PICO")
+    peak_1 = list_of_peaks(histogram[:midpoint],midpoint, flag=1)
+    #peak_2 = list_of_peaks(histogram[midpoint:],midpoint, flag=2)
     #print('1er pico elaborado:')
     #print(peak_1)
-    leftx_base = peak_1
+    #leftx_base = peak_1
+    #rightx_base = peak_2 + midpoint
 
 
     # Choose the number of sliding windows
@@ -287,12 +312,14 @@ def findng_lines(binary_warped):
     leftx_current = leftx_base
     rightx_current = rightx_base
     # Set the width of the windows +/- margin
-    margin = 100
+    margin = 50
     # Set minimum number of pixels found to recenter window
     minpix = 50
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
     right_lane_inds = []
+    sum_dados_left = []
+    sum_dados_right = []
 
     # Step through the windows one by one
     for window in range(nwindows):
@@ -321,6 +348,8 @@ def findng_lines(binary_warped):
             leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
         if len(good_right_inds) > minpix:
             rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+
 
     # Concatenate the arrays of indices
     left_lane_inds = np.concatenate(left_lane_inds)
@@ -355,7 +384,142 @@ def findng_lines(binary_warped):
 
     return left_lane_inds, right_lane_inds, left_fit, right_fit, out_img, nonzeroy, nonzerox, histogram
 
-images = glob.glob('test_images/*.jpg')
+def findng_lines_targeted(binary_warped,left_fit, right_fit):
+    # # Take a histogram of the bottom half of the image
+    # histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+    # # Create an output image to draw on and  visualize the result
+    # out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    # # Find the peak of the left and right halves of the histogram
+    # # These will be the starting point for the left and right lines
+    # midpoint = np.int(histogram.shape[0]/2)
+    # leftx_base = np.argmax(histogram[400:midpoint])+400
+    # rightx_base = np.argmax(histogram[midpoint:1060]) + midpoint
+    #
+    # #print('Pico raw izquerda')
+    # #print(leftx_base)
+    # #print("ENTRO PARA CALCULAR PICO")
+    # peak_1 = list_of_peaks(histogram[:midpoint],midpoint, flag=1)
+    # #peak_2 = list_of_peaks(histogram[midpoint:],midpoint, flag=2)
+    # #print('1er pico elaborado:')
+    # #print(peak_1)
+    # #leftx_base = peak_1
+    # #rightx_base = peak_2 + midpoint
+    #
+    #
+    # # Choose the number of sliding windows
+    # nwindows = 9
+    # # Set height of windows
+    # window_height = np.int(binary_warped.shape[0]/nwindows)
+    # # Identify the x and y positions of all nonzero pixels in the image
+    # nonzero = binary_warped.nonzero()
+    # nonzeroy = np.array(nonzero[0])
+    # nonzerox = np.array(nonzero[1])
+    # # Current positions to be updated for each window
+    # leftx_current = leftx_base
+    # rightx_current = rightx_base
+    # # Set the width of the windows +/- margin
+    # margin = 50
+    # # Set minimum number of pixels found to recenter window
+    # minpix = 50
+    # # Create empty lists to receive left and right lane pixel indices
+    # left_lane_inds = []
+    # right_lane_inds = []
+    #
+    # # Step through the windows one by one
+    # for window in range(nwindows):
+    #     # Identify window boundaries in x and y (and right and left)
+    #     win_y_low = binary_warped.shape[0] - (window+1)*window_height
+    #     win_y_high = binary_warped.shape[0] - window*window_height
+    #     win_xleft_low = leftx_current - margin
+    #     win_xleft_high = leftx_current + margin
+    #     win_xright_low = rightx_current - margin
+    #     win_xright_high = rightx_current + margin
+    #     # Draw the windows on the visualization image
+    #     cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
+    #     (0,255,0), 2)
+    #     cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
+    #     (0,255,0), 2)
+    #     # Identify the nonzero pixels in x and y within the window
+    #     good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+    #     (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+    #     good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+    #     (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+    #     # Append these indices to the lists
+    #     left_lane_inds.append(good_left_inds)
+    #     right_lane_inds.append(good_right_inds)
+    #     # If you found > minpix pixels, recenter next window on their mean position
+    #     if len(good_left_inds) > minpix:
+    #         leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+    #     if len(good_right_inds) > minpix:
+    #         rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+    #
+    # # Concatenate the arrays of indices
+    # left_lane_inds = np.concatenate(left_lane_inds)
+    # right_lane_inds = np.concatenate(right_lane_inds)
+    #
+    # # Extract left and right line pixel positions
+    # leftx = nonzerox[left_lane_inds]
+    # lefty = nonzeroy[left_lane_inds]
+    # rightx = nonzerox[right_lane_inds]
+    # righty = nonzeroy[right_lane_inds]
+    #
+    # # Fit a second order polynomial to each
+    # left_fit = np.polyfit(lefty, leftx, 2)
+    # right_fit = np.polyfit(righty, rightx, 2)
+
+    # Fit a second order polynomial in meters
+    #************************************************************************
+    # Assume you now have a new warped binary image
+    # from the next frame of video (also called "binary_warped")
+    # It's now much easier to find line pixels!
+    #************************************************************************
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy +
+    left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) +
+    left_fit[1]*nonzeroy + left_fit[2] + margin)))
+
+    right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy +
+    right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) +
+    right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    # END OF CODE OPTIMZED for this function without windowing solution
+    #
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+
+    # Calculate the new radii of curvature
+    y_eval = np.max(lefty)
+    y_evall = np.max(righty)
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_evall*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    # Now our radius of curvature is in meters
+    print('Left_line_curvature = ',left_curverad, 'm','   Right_line_curvature = ', right_curverad, 'm')
+
+
+    return left_lane_inds, right_lane_inds, left_fit, right_fit, nonzeroy, nonzerox, left_curverad, right_curverad
+
+images = []
+#images = glob.glob('test_images/*.jpg')
+#images = glob.glob('evaluation_images/*.jpg')
 #images = glob.glob('test_images/test5.jpg')
 for frame in images:
     print(frame)
@@ -370,11 +534,12 @@ for frame in images:
 
     #image = mpimg.imread('test_images/test1.jpg')
     # Applying Threshold
-    result_mixcolor, result_binary  = pipeline(image, s_thresh=(170, 255), sx_thresh=(20, 100), d_thresh = (0.75, 1.3), mag_thresh=(58,110))
+    result_mixcolor, result_binary  = pipeline(image, s_thresh=(100, 255), sx_thresh=(20, 100), d_thresh = (0.7, 1.3), mag_thresh=(58,110))
 
+    #binary_warped, perspective_M = corners_unwarp(result_binary, objpoints, imgpoints, src, dst)
     binary_warped, perspective_M = corners_unwarp(result_binary, objpoints, imgpoints, src, dst)
 
-    left_lane_inds, right_lane_inds, left_fit, right_fit, out_img, nonzeroy, nonzerox, histogram= findng_lines(binary_warped)
+    left_lane_inds, right_lane_inds, left_fit, right_fit, out_img, nonzeroy, nonzerox, histogram= findng_lines_slidingwindow(binary_warped)
 
     # Calculations of polynoms points
     ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
@@ -404,6 +569,7 @@ for frame in images:
 
     # ***********visualization
     # Generate x and y values for plotting
+    flag_plot = False
     if flag_plot == True:
         out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
         out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
@@ -435,13 +601,74 @@ for frame in images:
 # Video
 ################################################################################
 def process_image(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    return gray
+    global globalCounter
+    global left_fit
+    global right_fit
+    global left_fitx_mean
+    global right_fitx_mean
+    #gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    image = cal_undistort(img, objpoints, imgpoints)
+    point_0 = [587,452]
+    point_1 = [691,452]
+    point_2 = [200,718]
+    point_3 = [1120,718]
+
+    #image = mpimg.imread('test_images/test1.jpg')
+    # Applying Threshold
+    result_mixcolor, result_binary  = pipeline(image, s_thresh=(100, 255), sx_thresh=(20, 100), d_thresh = (0.75, 1.3), mag_thresh=(58,110))
+    print(globalCounter)
+    binary_warped, perspective_M = corners_unwarp(result_binary, objpoints, imgpoints, src, dst)
+    if globalCounter < 2:
+        left_lane_inds, right_lane_inds, left_fit, right_fit, out_img, nonzeroy, nonzerox, histogram= findng_lines_slidingwindow(binary_warped)
+        print("Using WINDOWING VERSION of FINDING LINES")
+    else:
+        left_lane_inds, right_lane_inds, left_fit, right_fit, nonzeroy, nonzerox, left_curverad, right_curverad = findng_lines_targeted(binary_warped,left_fit, right_fit)
+        print("Using OPTIMIZED VERSION of FINDING LINES")
+        if fabs(left_curverad-right_curverad) > 500:
+            globalCounter = 0
+    globalCounter += 1
+
+    # Calculations of polynoms points
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    print(type(left_fitx))
+    print(left_fit.size)
+    print(left_fitx_mean.size)
+    left_fitx_mean = left_fitx*0.1 + left_fitx_mean*0.9
+    right_fitx_mean = right_fitx*0.1 + right_fitx_mean*0.9
+
+    left_fitx = left_fitx_mean
+    right_fitx = right_fitx_mean
 
 
+    # Preparing and format for undistort with detected green region
+    warped = binary_warped
+        # Create an image to draw the lines on
+    warp_zero = np.zeros_like(warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
 
-video_output = 'outputvideo.mp4'
-clip1 = VideoFileClip("project_video.mp4")
-white_clip = clip1.fl_image(process_image)
-white_clip.write_videofile(video_output, audio=False)
-print('finished')
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+    Minv = cv2.getPerspectiveTransform(dst, src) # Inverse transformation
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0]))
+    # Combine the result with the original image
+    undist = image
+    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+    return result
+
+if True:
+    globalCounter = 0
+    left_fitx_mean = np.zeros(720)
+    right_fitx_mean = np.zeros(720)
+    video_output = 'outputvideo.mp4'
+    clip1 = VideoFileClip("project_video.mp4")
+    white_clip = clip1.fl_image(process_image).subclip(23,25)
+    white_clip.write_videofile(video_output, audio=False)
+    print('finished')
